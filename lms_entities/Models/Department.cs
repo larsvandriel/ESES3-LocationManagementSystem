b7010e98cbf40bd79217d3d9859586a0a333263c
larsvandriel <lars.van.driel@hotmail.com>
2021-12-30 23:47:38 +0100

    Create Models for LocationManagementSystem

---
+++ lms_entities/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationManagementSystem.Entities.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Employee HeadOfDepartment { get; set; }
+        public List<Employee> TeamLeads { get; set; }
+    }
+}
---
+++ lms_entities/Models/Location.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationManagementSystem.Entities.Models
+{
+    public class Location
+    {
+        public Guid Id { get; set; }
+        public int LocationNumber { get; set; }
+        public LocationType Type { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Employee SiteManager { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<Room> Rooms { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime DateDeleted { get; set; }
+    }
+}
